055cb7ae33391023c20823fbf28dc19b12f2d14b
Ollie Kavanagh <olliek@gmail.com>
2020-05-24 11:17:17 +0100

    Update colours for 57

--- src/components/waveform/index.js
+++ src/components/waveform/index.js
@@ -202,6 +202,9 @@ class WaveformProgress extends Component {
       case 56:
         color = '#3FD7F7';
         break;
+      case 57:
+        color = '#F45922';
+        break;
       default:
         color = '#ff6d00';
     }
